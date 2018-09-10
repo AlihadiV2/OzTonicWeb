90bd3dad53f0015c289a33e9a7b68ef703ec05a4
AlihadiV2 <43127295+AlihadiV2@users.noreply.github.com>
2018-09-11 00:16:04 +1000

    Add files via upload
    First Commit

---
+++ js/main.js
@@ -0,0 +1,329 @@
+/* start Table Slider*/
+function myFunction() {
+    "use strict";
+    var change = document.getElementById("btn-slideT");
+    if (change.innerHTML === "Hide Nutrition Information") {
+        change.innerHTML = "Show Nutrition Information";
+    } else {
+        change.innerHTML = "Hide Nutrition Information";
+    }
+}
+
+
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#btn-slideT").click(
+            function() {
+                $("#slide-table").slideToggle("slow");
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $('#slide-table').hide();
+});
+
+/* end Table Silder*/
+
+/*   Partner ifo Slider              */
+
+
+/* (((((((((((((((((((((((((((((           Partner 1 Slider                  ))))))))))))))))))))))))))))) */
+function partner1SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner1_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner1_btn").click(
+            function() {
+                $(".partner1Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $('.partner1Info').hide();
+});
+
+
+/* (((((((((((((((((((((((((((((           Partner 2 Slider                  ))))))))))))))))))))))))))))) */
+function partner2SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner2_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner2_btn").click(
+            function() {
+                $(".partner2Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $('.partner2Info').hide();
+});
+
+
+/* (((((((((((((((((((((((((((((           Partner 3 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner3SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner3_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner3_btn").click(
+            function() {
+                $(".partner3Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $('.partner3Info').hide();
+});
+
+
+/* (((((((((((((((((((((((((((((           Partner 4 Slider                  ))))))))))))))))))))))))))))) */
+function partner4SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner4_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner4_btn").click(
+            function() {
+                $(".partner4Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner4Info").hide();
+});
+
+
+
+/* (((((((((((((((((((((((((((((           Partner 5 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner5SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner5_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner5_btn").click(
+            function() {
+                $(".partner5Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner5Info").hide();
+});
+
+
+/* (((((((((((((((((((((((((((((           Partner 6 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner6SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner6_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner6_btn").click(
+            function() {
+                $(".partner6Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner6Info").hide();
+});
+
+
+/* (((((((((((((((((((((((((((((           Partner 7 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner7SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner7_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner7_btn").click(
+            function() {
+                $(".partner7Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner7Info").hide();
+});
+
+
+
+/* (((((((((((((((((((((((((((((           Partner 8 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner8SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner8_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner8_btn").click(
+            function() {
+                $(".partner8Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner8Info").hide();
+});
+
+
+
+
+/* (((((((((((((((((((((((((((((           Partner 6 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner9SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner9_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner9_btn").click(
+            function() {
+                $(".partner9Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner9Info").hide();
+});
+
+
+
+/* (((((((((((((((((((((((((((((           Partner 10 Slider                  ))))))))))))))))))))))))))))) */
+
+function partner10SliderInfo() {
+    "use strict";
+    var change = document.getElementById("partner10_btn");
+    if (change.innerHTML === "Hide Info") {
+        change.innerHTML = "Show Info";
+    } else {
+        change.innerHTML = "Hide Info";
+    }
+}
+
+$("document").ready(
+    function() {
+        "use strict";
+        $("#partner10_btn").click(
+            function() {
+                $(".partner10Info").slideToggle(1000);
+
+            });
+    });
+
+$(document).ready(function() {
+    "use strict";
+    $(".partner10Info").hide();
+});
+
+
+/*      Social media links stick to windows            */
+$("document").ready(function() {
+    var smOffset = $(".sm-links").offset().top;
+
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        if (scrollPos >= navOffset) {
+            $(".sm-links").addClass("sm-fixed");
+        } else {
+            $(".sm-links").removeClass("sm-fixed");
+        }
+    });
+});
